7e1483e6727b59f78224327ab7702a5c7ecc6255
achmeddieo17 <achmeddieom@gmail.com>
2023-09-17 22:52:35 +0800

    Latihan

---
+++ Hasil.js
@@ -0,0 +1,16 @@
+const prompt = require('prompt-sync')();
+const {hitungNilai} = require('./main.js');
+
+function getInput() {
+  const inputNilai = prompt('Masukkan nilai Anda (0-100): ');
+  const nilai = hitungNilai(inputNilai);
+
+  if (nilai === 'Invalid') {
+    console.log('Masukkan nilai antara 0 - 100.');
+    getInput(); 
+  } else {
+    console.log(`Nilai Anda : ${nilai}!`);
+  }
+}
+
+getInput(); 
---
+++ main.js
@@ -0,0 +1,26 @@
+// main.js
+
+function hitungNilai(inputNilai) {
+    inputNilai = inputNilai;
+  
+    if (!isNaN(inputNilai) && inputNilai >= 0 && inputNilai <= 100) {
+      if (inputNilai >= 90 && inputNilai <= 100) {
+        return 'A';
+      } else if (inputNilai >= 80 && inputNilai < 90) {
+        return 'B';
+      } else if (inputNilai >= 70 && inputNilai < 80) {
+        return 'C';
+      } else if (inputNilai >= 60 && inputNilai < 70) {
+        return 'D';
+      } else {
+        return 'F';
+      }
+    } else {
+      return 'Invalid';
+    }
+  }
+  
+  module.exports = {
+    hitungNilai: hitungNilai
+  };
+  
