aa5d09cad1dba80626f24132033fc82158bf8e75
achmeddieo17 <achmeddieom@gmail.com>
2023-09-19 12:52:22 +0800

    file terbaru

--- coba.spec.js
+++ coba.spec.js
@@ -3,66 +3,101 @@ const hitungNilai = require('./Hasil.js');
 
 describe('Konversi Nilai Mata Kuliah', function(){
     
-    describe('TESTCASE-ID-1 - Memasukan Niai 95', function () {
+    describe('TESTCASE-ID-1 - Memasukan Nilai 100', function () {
 
         it('Mendapatkan nilai A', function () {
-            const test = hitungNilai(95)
+            const test = hitungNilai(100)
             expect(test).to.be.equal('A')  
         })
     }) 
-    describe('TESTCASE-ID-2 - Memasukan Niai 80', function () {
+    describe('TESTCASE-ID-2 - Memasukan Nilai 90', function () {
+
+        it('Mendapatkan nilai A', function () {
+            const test = hitungNilai(90)      
+            expect(test).to.be.equal('A')  
+        })
+    })
+    describe('TESTCASE-ID-3 - Memasukan Nilai 89', function () {
+
+        it('Mendapatkan nilai B', function () {
+            const test = hitungNilai(89)       
+            expect(test).to.be.equal('B')  
+        })
+    })
+    describe('TESTCASE-ID-4 - Memasukan Nilai 80', function () {
 
         it('Mendapatkan nilai B', function () {
             const test = hitungNilai(80)      
             expect(test).to.be.equal('B')  
         })
-    })
-    describe('TESTCASE-ID-3 - Memasukan Niai 70', function () {
+    }) 
+    describe('TESTCASE-ID-5 - Memasukan Nilai 79', function () {
 
         it('Mendapatkan nilai C', function () {
-            const test = hitungNilai(70)       
+            const test = hitungNilai(79)     
             expect(test).to.be.equal('C')  
         })
-    })
-    describe('TESTCASE-ID-4 - Memasukan Niai 60', function () {
+    }) 
+    describe('TESTCASE-ID-6 - Memasukan Nilai 70', function () {
+
+        it('Mendapatkan nilai C', function () {
+            const test = hitungNilai(70)     
+            expect(test).to.be.equal('C')  
+        })
+    })    
+    describe('TESTCASE-ID-7 - Memasukan Nilai 69', function () {
 
         it('Mendapatkan nilai D', function () {
-            const test = hitungNilai(60)      
+            const test = hitungNilai(69)     
             expect(test).to.be.equal('D')  
         })
-    }) 
-    describe('TESTCASE-ID-5 - Memasukan Niai 50', function () {
+    })  
+    describe('TESTCASE-ID-8 - Memasukan Nilai 60', function () {
+
+        it('Mendapatkan nilai D', function () {
+            const test = hitungNilai(60)     
+            expect(test).to.be.equal('D')  
+        })
+    })   
+    describe('TESTCASE-ID-9 - Memasukan Nilai 59', function () {
 
         it('Mendapatkan nilai E', function () {
-            const test = hitungNilai(50)     
+            const test = hitungNilai(59)     
             expect(test).to.be.equal('E')  
         })
-    }) 
-    describe('TESTCASE-ID-6 - Memasukan Niai 30', function () {
+    })  
+    describe('TESTCASE-ID-10 - Memasukan Nilai 40', function () {
+
+        it('Mendapatkan nilai E', function () {
+            const test = hitungNilai(40)     
+            expect(test).to.be.equal('E')  
+        })
+    })  
+    describe('TESTCASE-ID-11 - Memasukan Nilai 30', function () {
 
         it('Mendapatkan nilai F', function () {
             const test = hitungNilai(30)     
             expect(test).to.be.equal('F')  
         })
-    })     
+    })  
 })
 
 describe ('Konversi Nilai Mata Kuliah Dengan Inputan Yang Salah', function () {
-    describe('TESTCASE-ID-7 - Memasukan Niai "halo semuanya"', function () {
+    describe('TESTCASE-ID-12 - Memasukan Nilai "halo semuanya"', function () {
 
         it('Masukkan nilai Anda (0-100)', function () {
             const test = hitungNilai("halo semuanya")       
             expect(test).to.be.equal('Invalid')  
         })
     }) 
-    describe('TESTCASE-ID-8 - Memasukan Niai 101', function () {
+    describe('TESTCASE-ID-13 - Memasukan Nilai 101', function () {
 
         it('Masukkan nilai Anda (0-100)', function () {
             const test = hitungNilai("101")      
             expect(test).to.be.equal('Invalid')  
         })
     })
-    describe('TESTCASE-ID-9 - Memasukan Niai -1', function () {
+    describe('TESTCASE-ID-14 - Memasukan Nilai -1', function () {
 
         it('Masukkan nilai Anda (0-100)', function () {
             const test = hitungNilai("-1")      
